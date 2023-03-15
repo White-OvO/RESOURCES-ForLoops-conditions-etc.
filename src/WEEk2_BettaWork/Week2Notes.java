9053d544c5a34a9254ce51e48368e1684798c4cf
White-OvO <Christophergonzalez1996@yahoo.com>
2023-03-15 16:29:02 -0700

    Week 2 of my promineo tech excerises

---
+++ src/WEEk2_BettaWork/Week2Notes.java
@@ -0,0 +1,56 @@
+package WEEk2_BettaWork;
+public class Week2Notes {
+	public static void main(String[] args) {
+	
+	
+	
+	  
+			int var1;             
+			int var2;
+			
+			/*varibles can also be mixed togther as ; int var1,var2;
+			*/
+			
+			var1 = 1024; 
+			
+			
+			System.out.println("var1 contains " + var1);
+			
+			var2 = var1 / 2;
+			
+			System.out.print ("Var2 contains var1 / 2: "); System.out.println(var2);
+System.out.println("HEllo WOrld");
+		
+			
+			/* There are two ways for a continued line or a new line
+			ex: System.out.print((xxx) This means there is not a new line 
+				System.out.println(xxx) means output for a new line. 
+				*/
+System.out.print("THis is an example for the comment above");
+var1 = 1024; 
+
+
+System.out.println("var1 contains " + var1);
+
+var2 = var1 / 2;
+
+System.out.println ("Var2 contains var1 / 2: "); System.out.println(var2);
+System.out.println("HEllo WOrld");
+
+int carspeed = 53;
+	if(carspeed > 55) {
+	 System.out.println( "you are over the speed limit!");
+	} else if ( carspeed == 55 ) {
+	    	System.out.println("Your are at the speed limit!");
+	} else {
+		System.out.println("Your holding traffic");
+
+	
+		
+	}
+	
+
+	
+}
+	
+	}
---
+++ src/WEEk2_BettaWork/week2_for_loops.java
@@ -0,0 +1,34 @@
+package WEEk2_BettaWork;
+
+public class week2_for_loops {
+
+	public static void main(String[] args) {
+
+	/*	for (int i = 0 ; i < 10; i++) {
+			System.out.println("SCOOP CUP OF FLOUR");
+			*/
+		
+		
+		// EX 2
+		// System.out.println("i");  //This command will only print i 
+		//System.out.println(i); //this will start at 0 and go to 9. because i is equal to one , one is less then two so it exucutes till it hits 10.
+		
+		
+//}
+		
+		
+		
+		/*for (int i = 1; i <= 10; i++) {
+				System.out.println(i); //This prints out 1-10.
+		*/
+	//	}
+	
+	
+	for (int i = 0; i < 100; i +=2 ) {
+		System.out.println(i);
+		}
+		}
+	
+}
+
+
